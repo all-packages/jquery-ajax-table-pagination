77aea572f30fd277da098b158e5e0cc4fb78ff9e
Prasanna Kumar <48315752+phpdeveloper798@users.noreply.github.com>
2019-03-12 03:05:19 +0530

    added examples for plugin

--- index.php
+++ index.php
@@ -15,46 +15,15 @@ and open the template in the editor.
     </head>
     <body>
         <div class="datatable">
-            <table>
-               <thead>
-               <tr>
-               <th>column1</th>
-               <th>column2</th>
-               <th>column3</th>
-               <th>column4</th>
-               </tr>
-               </thead>
-                <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>2</td>
-                        <td>3</td>
-                        <td>4</td>
-                    </tr>
-                    <tr>
-                        <td>5</td>
-                        <td>6</td>
-                        <td>7</td>
-                        <td>8</td>
-                    </tr>
-                    <tr>
-                        <td>9</td>
-                        <td>1</td>
-                        <td>2</td>
-                        <td>3</td>
-                    </tr>
-                    <tr>
-                        <td>4</td>
-                        <td>1</td>
-                        <td>6</td>
-                        <td>7</td>
-                    </tr>
-                </tbody>
-            </table>
         </div>
-        
         <div class="datatable1">
-            <table>
+        </div>
+        <div class="datatable2">
+        </div>
+        <div class="datatable3">
+        </div>
+        <div class="datatable4">
+        <table>
                <thead>
                <tr>
                <th>column1</th>
@@ -90,104 +59,32 @@ and open the template in the editor.
                     </tr>
                 </tbody>
             </table>
-            
-            
         </div>
         
-        <div class="datatable2">
-            <table>
-               <thead>
-               <tr>
-               <th>column1</th>
-               <th>column2</th>
-               <th>column3</th>
-               <th>column4</th>
-               </tr>
-               </thead>
-                <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>2</td>
-                        <td>3</td>
-                        <td>4</td>
-                    </tr>
-                    <tr>
-                        <td>5</td>
-                        <td>6</td>
-                        <td>7</td>
-                        <td>8</td>
-                    </tr>
-                    <tr>
-                        <td>9</td>
-                        <td>1</td>
-                        <td>2</td>
-                        <td>3</td>
-                    </tr>
-                    <tr>
-                        <td>4</td>
-                        <td>1</td>
-                        <td>6</td>
-                        <td>7</td>
-                    </tr>
-                </tbody>
-            </table>
-            
-            
-        </div>
         <script>
-            /*$(document).ready(function(){
-               $rows = $('table tbody');
-               $jsonTable = [];
-               $search = '4';
-               console.log($rows);
-               $columns = {};
-               $('table tbody tr').each(function(){
-                   $jsonrow = [];
-                   $i = 0;
-                   $match = false;
-                   $(this).children('td').each(function(){
-                      $jsonrow[$i] = $(this).text(); 
-                      $i++;
-                   });
-                   $jsonrow = $.grep($jsonrow, function( a ) {
-                            $regx = new RegExp($search);
-                            if(a.match($regx)){
-                                return true;
-                            }else{
-                                return false;
-                            }
-                         });
-                    if($jsonrow.length > 0){
-                           $jsonTable.push($jsonrow);
-                    }
-               });
-               console.log($jsonTable);
-            });*/
                $('.datatable').customeDataTable({
                    searchcolumns : [],
                    orderby : 0,
-                   headers : [],
-                   actionurl : "tablestructure.php",
+                   headers : ['Sno'],
+                   actionurl : "sampledata.php",
                    actiontype : "post",
                    tableprepare : "datatable",
                    perpage : 3,
-                   structure : 'local',
-                   //localpagination: false, // true or false it will use in full data get from server at on time.
-                   serverpagination: false // true or false
+                   structure : 'json'
                });
                
                $('.datatable1').customeDataTable({
                    tableIndex : 2,
                    searchcolumns : [],
                    orderby : 0,
-                   headers : [],
-                   actionurl : "tablestructure.php",
+                   headers : ['Sno'],
+                   actionurl : "sampledata.php",
                    actiontype : "post",
                    tableprepare : "datatable",
                    perpage : 2,
-                   structure : 'table',
+                   structure : 'json',
                    //localpagination: true, // true or false it will use in full data get from server at on time.
-                   //serverpagination: false // true or false
+                   serverpagination: false // true or false
                });
                
                $('.datatable2').customeDataTable({
@@ -199,8 +96,32 @@ and open the template in the editor.
                    actiontype : "post",
                    tableprepare : "datatable",
                    perpage : 1,
+                   structure : 'table'
+               });
+
+               $('.datatable3').customeDataTable({
+                   tableIndex : 4,
+                   searchcolumns : [],
+                   orderby : 0,
+                   headers : [],
+                   actionurl : "tablestructure.php",
+                   actiontype : "post",
+                   tableprepare : "datatable",
+                   perpage : 1,
+                   structure : 'table',
+                   serverpagination: false // true or false
+               });
+
+               $('.datatable4').customeDataTable({
+                   tableIndex : 5,
+                   searchcolumns : [],
+                   orderby : 0,
+                   headers : [],
+                   actionurl : "",
+                   actiontype : "",
+                   tableprepare : "",
+                   perpage : 1,
                    structure : 'local',
-                   //localpagination: true, // true or false it will use in full data get from server at on time.
                    serverpagination: false // true or false
                });
                
